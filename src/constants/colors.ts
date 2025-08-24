e547e49f041baeb3f3432b17dc29a77fed30ab4b
m4c1elz <milaelipe.maciel@gmail.com>
2025-08-24 14:39:05 -0300

    feat(cli): add chart generation for likes/views on view command

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,3 +2,6 @@ import chalk from 'chalk'
 
 export const primaryColorText = chalk.hex('#f8d13e')
 export const secondaryColorText = chalk.hex('#f96c39')
+
+export const primaryColorHex = '#f8d13e'
+export const secondaryColorHex = '#f96c39'
